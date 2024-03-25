1caaa83b4bf695433134dfaafeb7262a920caf32
Johanlefrere <johan.lefrere0928@gmail.com>
2024-03-25 15:45:27 +0100

    début

---
+++ accueil.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link href="bootstrap.css" rel="stylesheet">
+    <link href="accueilCSS.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div class="container-fluid">
+                <img src="logo.png" alt="logo" style="width:4%; height:auto" />
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active text" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Education</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</body>
+
+</html>
