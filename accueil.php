e8209e429be9155fd29c2b24a3ce6d7ab392ffb0
Johanlefrere <johan.lefrere0928@gmail.com>
2024-03-26 10:48:35 +0100

    page d'accueil
    il manque le footer, je n'arrive pas à mettre les boutons dans les paragraphe.

--- accueil.php
+++ accueil.php
@@ -5,15 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
+    <link href="test.css" rel="stylesheet">
     <link href="bootstrap.css" rel="stylesheet">
-    <link href="accueilCSS.css" rel="stylesheet">
+
 </head>
 
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <div class="container-fluid">
-                <img src="logo.png" alt="logo" style="width:4%; height:auto" />
+                <img src="img/logo.png" alt="logo" style="width:50px; height:auto" />
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
@@ -30,6 +31,73 @@
             </div>
         </nav>
     </header>
+
+    <div class="container-fluid" id="firstContainer">
+        <div class="row width:100%">
+            <div class="col-lg-7">
+                <div class="container">
+                    <img src="img/pointillé.png" class="vecteur rounded mx-auto d-block" />
+                </div>
+                <div class="container">
+                    <h2>Couture & Apprentissage :<br>
+                        Fournitures et Cours Experts
+                    </h2>
+                    <div class="d-flex justify-content-center">
+                        <p class="paragraphe">Découvrez notre sélection de fournitures et cours experts<br> pour exprimer votre créativité à travers la couture.</p>
+                    </div>
+                    <div class="d-flex justify-content-center bouttonSouscrire">
+                        <button class="btn btn-dark">Souscrire à cette offre</button>
+                    </div>
+
+                </div>
+                <div class="container">
+                    <img src="img/pointillé.png" class="vecteur rounded mx-auto d-block" />
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <img src="img/vetements.png" />
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid text-center">
+        <br><br>
+        <p>NOS CLIENTS</p>
+        <h3>Ce qu'ils pensent de nous</h3>
+        <div class="row justify-content-center">
+            <div class="paragrapheClients col-lg-2 shadow p-3 mb-5 bg-white rounded">
+                <p>htrhuthut tjiouthhugituith jgriuhigthgui thuguhihughug hugthuighuigthuigt gthuighuithugthug gtghuithughugh uhithgh</p>
+            </div>
+            <div class="paragrapheClients col-lg-2 shadow p-3 mb-5 bg-white rounded">
+                <p>hirttuti tjigtiuuti tiutuituut tuiru ru urtuyio  oip"ép   "éou  u'o u 'o u'u "'op&^gijg ero ui </p>
+            </div>
+            <div class="paragrapheClients col-lg-2 shadow p-3 mb-5 bg-white rounded">
+                <p>tiotiorjitjo itjoitoit triotri triotiotioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr hh rh hrh hrhhrh hh rh hr hhrhr </p>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid text-center" id="troisiemeContainer">
+        <br><br>
+        <p>SERVICES</p>
+        <h3>Des offres adaptées à vous</h3>
+        <div class="row justify-content-center">
+            <div class="col-lg-3">
+                <img class="mx-auto d-block img" src="img/coutureDébutant.png"/>
+                <p class="shadow p-3 mb-5 bg-white img">rgnn  ie ii irri yziqoz r rrezèr rklrekj otioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr rer j k</p>
+                <button class="btn btn-primary">En savoir Plus</button>
+            </div>
+            <div class="col-lg-3">
+                <img class="mx-auto d-block img" src="img/couturePatrons.png"/>
+                <p class="shadow p-3 mb-5 bg-white img">rgnn rklrotioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr  otioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr ekj rer j k</p>
+                <button class="btn btn-primary">En savoir Plus</button>
+            </div>
+            <div class="col-lg-3">
+                <img class="mx-auto d-block img" src="img/coutureAvancée.png"/>
+                <p class="shadow p-3 mb-5 bg-white img">rgnn rklrotioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr  otioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr ekj rer j k</p>
+                <button class="btn btn-primary">En savoir Plus</button>
+            </div>
+        </div>
+    </div>
+    <footer></footer>
 </body>
 
 </html>
