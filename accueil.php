37ca08a5c6668bcd6f93ffc95dc3f4b9d1fa8a97
Johanlefrere <johan.lefrere0928@gmail.com>
2024-03-26 12:55:58 +0100

    page d'accueil
    footer

--- accueil.php
+++ accueil.php
@@ -75,7 +75,7 @@
                 <p>htrhuthut tjiouthhugituith jgriuhigthgui thuguhihughug hugthuighuigthuigt gthuighuithugthug gtghuithughugh uhithgh</p>
             </div>
             <div class="paragrapheClients col-lg-2 shadow p-3 mb-5 bg-white rounded">
-                <p>hirttuti tjigtiuuti tiutuituut tuiru ru urtuyio  oip"ép   "éou  u'o u 'o u'u "'op&^gijg ero ui </p>
+                <p>hirttuti tjigtiuuti tiutuituut tuiru ru urtuyio oip"ép "éou u'o u 'o u'u "'op&^gijg ero ui </p>
             </div>
             <div class="paragrapheClients col-lg-2 shadow p-3 mb-5 bg-white rounded">
                 <p>tiotiorjitjo itjoitoit triotri triotiotioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr hh rh hrh hrhhrh hh rh hr hhrhr </p>
@@ -87,24 +87,63 @@
         <p>SERVICES</p>
         <h3>Des offres adaptées à vous</h3>
         <div class="row justify-content-center">
-            <div class="col-lg-3">
-                <img class="mx-auto d-block img" src="img/coutureDébutant.png"/>
-                <p class="shadow p-3 mb-5 bg-white img">rgnn  ie ii irri yziqoz r rrezèr rklrekj otioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr rer j k</p>
+            <div class="col-lg-3 div">
+                <img class="mx-auto d-block img" src="img/coutureDébutant.png" />
+                <h3 class="p-3 mb-5 bg-white">Couture pour débutants</h3>
                 <button class="btn btn-primary">En savoir Plus</button>
             </div>
             <div class="col-lg-3">
-                <img class="mx-auto d-block img" src="img/couturePatrons.png"/>
-                <p class="shadow p-3 mb-5 bg-white img">rgnn rklrotioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr  otioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr ekj rer j k</p>
+                <img class="mx-auto d-block img" src="img/couturePatrons.png" />
+                <h3 class="p-3 mb-5 bg-white">Couture avec patrons</h3>
                 <button class="btn btn-primary">En savoir Plus</button>
             </div>
             <div class="col-lg-3">
-                <img class="mx-auto d-block img" src="img/coutureAvancée.png"/>
-                <p class="shadow p-3 mb-5 bg-white img">rgnn rklrotioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr  otioi hhhhhhhhhh hrhrhhrhr rhrhrhtht hrhrhh h hrhrr ekj rer j k</p>
+                <img class="mx-auto d-block img" src="img/coutureAvancée.png" />
+                <h3 class="p-3 mb-5 bg-white">Couture avancée</h3>
                 <button class="btn btn-primary">En savoir Plus</button>
             </div>
         </div>
     </div>
-    <footer></footer>
+    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+        
+        <div class="col mb-3">
+            <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+                <svg class="bi me-2" width="40" height="32">
+                    <use xlink:href="#bootstrap"></use>
+                </svg>
+            </a>
+            <p class="text-body-secondary">© 2024</p>
+        </div>
+            
+        <div class="col mb-3">
+
+        </div>
+
+        <div class="col mb-3">
+            <h5>LearnMore</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Environment</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Jobs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Privaty Policy</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Contact Us</a></li>
+            </ul>
+        </div>
+
+        <div class="col mb-3">
+            <h5>Contact Us</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Reservation : 12-34-56-78-90</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Accueil : 09-87-65-43-21</a></li>
+            </ul>
+        </div>
+        <div class="col mb-3">
+            <h5>Social</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+            </ul>
+        </div>
+    </footer>
 </body>
 
 </html>
