0f84e9b181b04c5fd3927c5b82c5f2ca8aa9a0d1
adamhassani <mohamed.hassani@etu.unilim.fr>
2024-03-26 12:29:39 +0100

    Navbar creee
    Il reste la couleur du bouton s'inscrire a changer

--- accueil.php
+++ accueil.php
@@ -4,28 +4,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
+    <title>Couture For Fun</title>
     <link href="bootstrap.css" rel="stylesheet">
-    <link href="accueilCSS.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
 </head>
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <img src="logo.png" alt="logo" style="width:4%; height:auto" />
-                <div class="collapse navbar-collapse" id="navbarNav">
+                <a class="navbar-brand" href="#"><img src="logo.png" width="50px" height="50px" alt="Logo"></a>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active text" aria-current="page" href="#">Home</a>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="#">Accueil</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Education</a>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="#">Nos Cours</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Contacts</a>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="#">Tarifs</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="#">Contacts</a>
                         </li>
                     </ul>
+                </div>
+                <div class="d-flex">
+                    <button class="btn ">Se connecter</button>
+                    <button class="btn rounded-pill">S'inscrire</button>
                 </div>
             </div>
         </nav>
