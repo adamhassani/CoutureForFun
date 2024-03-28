7a0cc33054de6033f75f7d540eec31a2e611eefe
adamhassani <mohamed.hassani@etu.unilim.fr>
2024-03-28 01:20:41 +0100

    minor navbar update
    More responsive navbar with bootstrap JS

--- inscription.php
+++ inscription.php
@@ -8,6 +8,7 @@
     <link href="bootstrap.css" rel="stylesheet">
     <link href="cssinscription/style1.css" rel="stylesheet">
     <link rel="icon" type="image/x-icon" sizes="64x64" href="img/logo1.png">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body class="bg-primary">
--- navbar.php
+++ navbar.php
@@ -1,31 +1,36 @@
 <header>
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
-            <div class="container-fluid">
-                <img src="img/logo1.png" alt="logo" style="width:50px; height:auto" />
-                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item mx-3">
-                            <a class="nav-link" href="accueil.php">Accueil</a>
-                        </li>
-                        <li class="nav-item mx-3">
-                            <a class="nav-link" href="#">Nos Cours</a>
-                        </li>
-                        <li class="nav-item mx-3">
-                            <a class="nav-link" href="#">Tarifs</a>
-                        </li>
-                        <li class="nav-item mx-3">
-                            <a class="nav-link" href="#">Contacts</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="d-flex">
-                    <a href="connection.php">
-                        <button class="btn" href="inscription.php">Se connecter</button>
-                    </a>
-                    <a href="inscription.php">
-                        <button class="btn btn-blue rounded-pill">S'inscrire</button>
-                    </a>
-                </div>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo1.png" alt="logo" style="width:50px; height:auto" class="ms-5"/>
+        </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="accueil.php">Accueil</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="#">Nos Cours</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="#">Tarifs</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="#">Contacts</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-    </header>
+            <div class="d-flex me-5">
+                <a href="connection.php">
+                    <button class="btn" href="inscription.php">Se connecter</button>
+                </a>
+                <a href="inscription.php">
+                    <button class="btn btn-blue rounded-pill">S'inscrire</button>
+                </a>
+            </div>
+        </div>
+    </nav>
+</header>
