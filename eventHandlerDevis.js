fd195bfb6e787f1d7db93ac7e9329b5fad4c9b7a
0xTristo <aliterro588@gmail.com>
2024-04-07 01:20:52 +0200

    mergeUpdate

--- eventHandlerDevis.js
+++ eventHandlerDevis.js
@@ -8,8 +8,27 @@ function devisValidation()
     var typevetIndex = typevet.selectedIndex;
 
     var tissuvet = document.getElementById("tissuvet");
-    var tissevetIndex = tissuvet.selectedIndex;
+    var tissuvetIndex = tissuvet.selectedIndex;
 
     var taillevet = document.getElementById("taillevet");
     var taillevetIndex = taillevet.selectedIndex;
+
+    var servicesInputs = document.getElementsByName('btnOptions');
+
+    // Initialize a variable to store the selected radio value
+    var services = null;
+    var coutServices = 0;
+    // Iterate over the radio inputs
+    for (var i = 0; i < servicesInputs.length; i++) {
+        // Check if the current radio input is checked
+        if (servicesInputs[i].checked) {
+            // Get the value of the checked radio input
+            services = servicesInputs[i].value;
+            break;
+        }
+    }
+
+    if (servies = )
+    var cout = 10*(typevetIndex + 1) + 20*(tissuvetIndex + 1) + 5*(taillevetIndex + 1) + 
+     
 }
